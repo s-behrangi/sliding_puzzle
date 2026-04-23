17362456aaada520f131b9944512b27a2fabb285
S Behrangi <eris.elea@gmail.com>
2026-04-23 19:25:14 -0400

    Update vite config base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` 
+    : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
